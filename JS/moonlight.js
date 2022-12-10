4fde38c393a51abd0723b9929218b2293ccb5fdb
belforz <macedobeiramar@hotmail.com>
2022-12-09 22:42:40 -0300

    Estruturação VII - Adicionamento de JS FINAL
    

---
+++ JS/moonlight.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+}
+
+alert("Professor, não consegui fazer com que certos elementos ficassem bloqueados para scroll mesmo usando background-attachment para isso")

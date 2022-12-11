7b5f421c6e0a6570193b18e96de0810bcc74a414
belforz <macedobeiramar@hotmail.com>
2022-12-11 16:10:51 -0300

    Estruturação VII - FINAL

--- JS/moonlight.js
+++ JS/moonlight.js
@@ -5,11 +5,11 @@ function myFunction() {
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
+      btnText.innerHTML = "Leia mais"; 
       moreText.style.display = "none";
     } else {
       dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
+      btnText.innerHTML = "Leia menos"; 
       moreText.style.display = "inline";
     }
 }
